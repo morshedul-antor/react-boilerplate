9a0ad15551d61e0e9939ce20c227294bb1064a89
morshedul-antor <antor.morshedul@gmail.com>
2022-10-01 11:08:18 +0600

    boilerplate: final setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div>
-      <h1>React Boilerplate</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>React Boilerplate</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
