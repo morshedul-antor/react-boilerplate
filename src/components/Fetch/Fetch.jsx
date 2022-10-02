f8d196b2b2569fbdb2001bf09d95a2d7ee54fd9e
morshedul-antor <antor.morshedul@gmail.com>
2022-10-02 17:48:51 +0600

    change: props add with style object

--- src/components/Fetch/Fetch.jsx
+++ src/components/Fetch/Fetch.jsx
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import classes from './Fetch.module.css'
 
-export default function Fetch() {
+export default function Fetch(props) {
     const [userInfo, setUserInfo] = useState({})
     const [repositories, setRepositories] = useState([])
+    const [isOpen, setIsOpen] = useState(true)
 
     const userName = 'morshedul-antor'
+
+    const styles = {
+        s1: {
+            color: 'var(--text)',
+            // backgroundColor: 'grey',
+        },
+        s2: {
+            color: 'red',
+        },
+    }
 
     useEffect(() => {
         let controller = new AbortController()
@@ -45,15 +56,23 @@ export default function Fetch() {
 
     return (
         <div className={classes.fetch}>
+            {isOpen && (
+                <>
+                    <div>
+                        <h1 style={styles.s1}>{props.title}</h1>
+                        <img src={userInfo.avatar_url} alt="" />
+                        <h2>{userInfo.name}</h2>
+                        <span>{userInfo.company}</span>
+                    </div>
+                    <div>
+                        {repositories.map((repo) => (
+                            <p>{repo.name}</p>
+                        ))}
+                    </div>
+                </>
+            )}
             <div>
-                <img src={userInfo.avatar_url} alt="" />
-                <h2>{userInfo.name}</h2>
-                <span>{userInfo.company}</span>
-            </div>
-            <div>
-                {repositories.map((repo) => (
-                    <p>{repo.name}</p>
-                ))}
+                <button onClick={() => setIsOpen(!isOpen)}>{isOpen === false ? 'Toggle On' : 'Toggle Off'}</button>
             </div>
         </div>
     )
